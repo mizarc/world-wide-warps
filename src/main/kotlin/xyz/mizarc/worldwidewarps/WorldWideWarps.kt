6a1230b1f2dcdf97430f9c9ff30a160364157fbb
Mizarc <mizarc@protonmail.com>
2022-01-03 20:41:20 +1000

    Add default config values
    These values will be used to dynamically configure certain elements for
    teleportation.

--- src/main/kotlin/xyz/mizarc/worldwidewarps/WorldWideWarps.kt
+++ src/main/kotlin/xyz/mizarc/worldwidewarps/WorldWideWarps.kt
@@ -9,12 +9,25 @@ class WorldWideWarps: JavaPlugin() {
 
     override fun onEnable() {
         commandManager = PaperCommandManager(this)
+        dataFolder.mkdir()
+        createConfig()
         registerCommands()
         logger.info("WorldWideWarps has been Enabled")
     }
 
     override fun onDisable() {
         logger.info("WorldWideWarps has been Disabled")
+    }
+
+    private fun createConfig() {
+        config.addDefault("home_default_to_spawn", true)
+        config.addDefault("home_timer", 5)
+        config.addDefault("home_cooldown", 30)
+        config.addDefault("spawn_timer", 5)
+        config.addDefault("spawn_cooldown", 30)
+        config.addDefault("invite_range", 20)
+        config.addDefault("tpr_radius", 1000)
+        config.addDefault("tpr_attempts", 50)
     }
 
     private fun registerCommands() {
