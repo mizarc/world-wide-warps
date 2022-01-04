3b4677578a75db97f87d5195f0402a168cc80dfb
Mizarc <mizarc@protonmail.com>
2022-01-04 21:39:21 +1000

    Add home array to player state
    This keeps a list of homes related to the player in memory.

--- src/main/kotlin/xyz/mizarc/worldwidewarps/PlayerState.kt
+++ src/main/kotlin/xyz/mizarc/worldwidewarps/PlayerState.kt
@@ -2,7 +2,7 @@ package xyz.mizarc.worldwidewarps
 
 import org.bukkit.entity.Player
 
-class PlayerState(var player: Player) {
+class PlayerState(val player: Player, val homes: Array<Home>) {
     var teleportTask: TeleportTask? = null
     var teleportCooldownTimer = 0
     var invitePlayer: Player? = null
