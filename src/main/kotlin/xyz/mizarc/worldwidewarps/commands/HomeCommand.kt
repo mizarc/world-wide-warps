dfbf02a51b360edce1949b2a46cafb4c01ba3c0d
Mizarc <mizarc@protonmail.com>
2022-01-04 14:23:39 +1000

    Change home command to use config values
    Config values are utilised to handle the timer, plus an additional
    permission check to bypass the teleport timer.

--- src/main/kotlin/xyz/mizarc/worldwidewarps/commands/HomeCommand.kt
+++ src/main/kotlin/xyz/mizarc/worldwidewarps/commands/HomeCommand.kt
@@ -7,24 +7,35 @@ import co.aikar.commands.annotation.Default
 import co.aikar.commands.annotation.Dependency
 import org.bukkit.configuration.file.FileConfiguration
 import org.bukkit.entity.Player
+import xyz.mizarc.worldwidewarps.Config
+import xyz.mizarc.worldwidewarps.PlayerContainer
 import xyz.mizarc.worldwidewarps.TeleportMessage
 import xyz.mizarc.worldwidewarps.Teleporter
 
 @CommandAlias("home")
 @CommandPermission("worldwidewarps.command.home")
 class HomeCommand: BaseCommand() {
-    @Dependency lateinit var config: FileConfiguration
+    @Dependency lateinit var config: Config
     @Dependency lateinit var teleporter: Teleporter
+    @Dependency lateinit var playerContainer: PlayerContainer
 
     @Default
     fun onHome(player: Player) {
+        // Get the player's bed location
         val bedSpawnLocation = player.bedSpawnLocation
         if (bedSpawnLocation == null) {
             player.sendMessage("§cYou don't have a home. Sleep in a bed to set your home.")
             return
         }
 
-        val timer = config.getInt("home_timer")
+        // Bypasses the cooldown timer if player has permission
+        if (player.hasPermission("worldwidewarps.teleport.home.bypasstimer")) {
+            teleporter.teleport(player, bedSpawnLocation, 0, TeleportMessage.HOME)
+            return
+        }
+
+        // Teleport the player
+        val timer = config.homeTimer
         player.sendMessage("§aTeleporting home, please wait §6$timer §aseconds.")
         teleporter.teleport(player, bedSpawnLocation, timer, TeleportMessage.HOME)
     }
