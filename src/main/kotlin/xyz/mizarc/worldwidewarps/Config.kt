67ffd77b176c918f7896b743020ab2be17e2df91
Mizarc <mizarc@protonmail.com>
2022-01-04 14:20:17 +1000

    Add config handler to save memory values to memory
    This way of handling configs allows commands and events to not have to
    fetch the config values from file all the time, saving cpu resources,
    while using a very minimal amount of memory in the process. This means
    that the config can't be updated live, but an alternative solution will
    be implemented later.

---
+++ src/main/kotlin/xyz/mizarc/worldwidewarps/Config.kt
@@ -0,0 +1,53 @@
+package xyz.mizarc.worldwidewarps
+
+import org.bukkit.Location
+import org.bukkit.plugin.Plugin
+import xyz.mizarc.worldwidewarps.utils.LocationConversions
+
+class Config(val plugin: Plugin) {
+    private val configFile = plugin.config
+
+    var spawnLocation: String = ""
+    var spawnTimer = 0
+    var spawnCooldown = 0
+    var homeDefaultToSpawn = false
+    var homeTimer = 0
+    var homeCooldown = 0
+    var inviteRange = 0
+
+    init {
+        createDefaultConfig()
+        loadConfig()
+    }
+
+    fun loadConfig() {
+        spawnTimer = configFile.getInt("spawn_timer")
+        spawnCooldown = configFile.getInt("spawn_cooldown")
+        homeDefaultToSpawn = configFile.getBoolean("home_default_to_spawn")
+        homeTimer = configFile.getInt("home_timer")
+        homeCooldown = configFile.getInt("home_cooldown")
+        inviteRange = configFile.getInt("invite_range")
+        spawnLocation = configFile.getString("spawn_location")!!
+    }
+
+    fun setSpawnLocation(location: Location) {
+        val spawnLocationString = LocationConversions.locationToString(location)
+        spawnLocation = spawnLocationString
+        configFile.set("spawn_location", spawnLocationString)
+        plugin.saveConfig()
+    }
+
+    private fun createDefaultConfig() {
+        plugin.config.addDefault("spawn_location" , "")
+        plugin.config.addDefault("spawn_timer", 5)
+        plugin.config.addDefault("spawn_cooldown", 30)
+        plugin.config.addDefault("home_default_to_spawn", true)
+        plugin.config.addDefault("home_timer", 5)
+        plugin.config.addDefault("home_cooldown", 30)
+        plugin.config.addDefault("invite_range", 20)
+        plugin.config.addDefault("tpr_radius", 1000)
+        plugin.config.addDefault("tpr_attempts", 50)
+        plugin.config.options().copyDefaults(true)
+        plugin.saveConfig()
+    }
+}
--- src/main/kotlin/xyz/mizarc/worldwidewarps/WorldWideWarps.kt
+++ src/main/kotlin/xyz/mizarc/worldwidewarps/WorldWideWarps.kt
@@ -5,17 +5,19 @@ import org.bukkit.configuration.file.FileConfiguration
 import org.bukkit.entity.Player
 import org.bukkit.plugin.java.JavaPlugin
 import xyz.mizarc.worldwidewarps.commands.HomeCommand
+import xyz.mizarc.worldwidewarps.commands.SetspawnCommand
+import xyz.mizarc.worldwidewarps.commands.SpawnCommand
 import xyz.mizarc.worldwidewarps.events.PlayerRegistrationListener
 
 class WorldWideWarps: JavaPlugin() {
     private lateinit var commandManager: PaperCommandManager
+    private val config = Config(this)
     val playerContainer = PlayerContainer()
     val teleporter = Teleporter(this, playerContainer)
 
     override fun onEnable() {
         commandManager = PaperCommandManager(this)
         dataFolder.mkdir()
-        createConfig()
         registerDependencies()
         registerCommands()
         registerEvents()
@@ -26,26 +28,17 @@ class WorldWideWarps: JavaPlugin() {
         logger.info("WorldWideWarps has been Disabled")
     }
 
-    private fun createConfig() {
-        config.addDefault("home_default_to_spawn", true)
-        config.addDefault("home_timer", 5)
-        config.addDefault("home_cooldown", 30)
-        config.addDefault("spawn_timer", 5)
-        config.addDefault("spawn_cooldown", 30)
-        config.addDefault("invite_range", 20)
-        config.addDefault("tpr_radius", 1000)
-        config.addDefault("tpr_attempts", 50)
-        config.options().copyDefaults(true)
-        saveConfig()
-    }
 
     private fun registerDependencies() {
+        commandManager.registerDependency(Config::class.java, config)
         commandManager.registerDependency(PlayerContainer::class.java, playerContainer)
         commandManager.registerDependency(Teleporter::class.java, teleporter)
     }
 
     private fun registerCommands() {
         commandManager.registerCommand(HomeCommand())
+        commandManager.registerCommand(SpawnCommand())
+        commandManager.registerCommand(SetspawnCommand())
     }
 
     private fun registerEvents() {
