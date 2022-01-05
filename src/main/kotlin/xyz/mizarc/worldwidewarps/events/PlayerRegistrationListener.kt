ab15ecd80fadaae0bde77b6108398044c77509e5
Mizarc <mizarc@protonmail.com>
2022-01-05 13:01:33 +1000

    Add required bed array to state on join
    On player join, the player's beds will be fetched from the database to
    be cached in their player state.

--- src/main/kotlin/xyz/mizarc/worldwidewarps/events/PlayerRegistrationListener.kt
+++ src/main/kotlin/xyz/mizarc/worldwidewarps/events/PlayerRegistrationListener.kt
@@ -4,14 +4,15 @@ import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.event.player.PlayerQuitEvent
+import xyz.mizarc.worldwidewarps.DatabaseStorage
 import xyz.mizarc.worldwidewarps.PlayerContainer
 import xyz.mizarc.worldwidewarps.PlayerState
 
-class PlayerRegistrationListener(val playerContainer: PlayerContainer): Listener {
+class PlayerRegistrationListener(val databaseStorage: DatabaseStorage, val playerContainer: PlayerContainer): Listener {
 
     @EventHandler
     fun onPlayerJoin(event: PlayerJoinEvent) {
-        val playerState = PlayerState(event.player)
+        val playerState = PlayerState(event.player, databaseStorage.getHomes(event.player))
         playerContainer.addPlayer(playerState)
     }
 
