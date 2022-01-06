4262b8c958acb95a7b4cf330bad0082e98d03788
Mizarc <mizarc@protonmail.com>
2022-01-06 21:29:09 +1000

    Change colour conversion to use extensions
    This makes it more convenient to use as it doesn't require a call to
    both a class and function, and instead can be called directly from the
    original class.

--- src/main/kotlin/xyz/mizarc/worldwidewarps/utils/BedColourConversion.kt
+++ src/main/kotlin/xyz/mizarc/worldwidewarps/utils/BedColourConversion.kt
@@ -2,28 +2,47 @@ package xyz.mizarc.worldwidewarps.utils
 
 import org.bukkit.DyeColor
 import org.bukkit.Material
+import org.bukkit.block.data.type.Bed
 
-class BedColourConversion {
-    companion object {
-        private val beds = mapOf(
-            DyeColor.BLACK to Material.BLACK_BED,
-            DyeColor.BLUE to Material.BLUE_BED,
-            DyeColor.CYAN to Material.CYAN_BED,
-            DyeColor.GRAY to Material.GRAY_BED,
-            DyeColor.GREEN to Material.GREEN_BED,
-            DyeColor.LIGHT_BLUE to Material.LIGHT_BLUE_BED,
-            DyeColor.LIGHT_GRAY to Material.LIGHT_GRAY_BED,
-            DyeColor.LIME to Material.LIME_BED,
-            DyeColor.MAGENTA to Material.MAGENTA_BED,
-            DyeColor.ORANGE to Material.ORANGE_BED,
-            DyeColor.PINK to Material.PINK_BED,
-            DyeColor.PURPLE to Material.PURPLE_BED,
-            DyeColor.RED to Material.RED_BED,
-            DyeColor.WHITE to Material.WHITE_BED,
-            DyeColor.YELLOW to Material.YELLOW_BED)
+fun DyeColor.toBed(): Material {
+    return when (this) {
+        DyeColor.BLACK -> Material.BLACK_BED
+        DyeColor.WHITE -> Material.WHITE_BED
+        DyeColor.ORANGE -> Material.ORANGE_BED
+        DyeColor.MAGENTA -> Material.MAGENTA_BED
+        DyeColor.LIGHT_BLUE -> Material.LIGHT_BLUE_BED
+        DyeColor.YELLOW -> Material.YELLOW_BED
+        DyeColor.LIME -> Material.LIME_BED
+        DyeColor.PINK -> Material.PINK_BED
+        DyeColor.GRAY -> Material.GRAY_BED
+        DyeColor.LIGHT_GRAY -> Material.LIGHT_GRAY_BED
+        DyeColor.CYAN -> Material.CYAN_BED
+        DyeColor.PURPLE -> Material.PURPLE_BED
+        DyeColor.BLUE -> Material.BLUE_BED
+        DyeColor.BROWN -> Material.BROWN_BED
+        DyeColor.GREEN -> Material.GREEN_BED
+        DyeColor.RED -> Material.RED_BED
+    }
+}
 
-        fun getBedMaterial(dyeColor: DyeColor): Material {
-            return if (beds[dyeColor] != null) beds[dyeColor]!! else Material.WHITE_BED
-        }
+fun Bed.getColour(): DyeColor {
+    return when (this.material) {
+        Material.BLACK_BED -> DyeColor.BLACK
+        Material.WHITE_BED -> DyeColor.WHITE
+        Material.ORANGE_BED -> DyeColor.ORANGE
+        Material.MAGENTA_BED -> DyeColor.MAGENTA
+        Material.LIGHT_BLUE_BED -> DyeColor.LIGHT_BLUE
+        Material.YELLOW_BED -> DyeColor.YELLOW
+        Material.LIME_BED -> DyeColor.LIME
+        Material.PINK_BED -> DyeColor.PINK
+        Material.GRAY_BED -> DyeColor.GRAY
+        Material.LIGHT_GRAY_BED -> DyeColor.LIGHT_GRAY
+        Material.CYAN_BED -> DyeColor.CYAN
+        Material.PURPLE_BED -> DyeColor.PURPLE
+        Material.BLUE_BED -> DyeColor.BLUE
+        Material.BROWN_BED -> DyeColor.BROWN
+        Material.GREEN_BED -> DyeColor.GREEN
+        Material.RED_BED -> DyeColor.RED
+        else -> DyeColor.WHITE
     }
 }
