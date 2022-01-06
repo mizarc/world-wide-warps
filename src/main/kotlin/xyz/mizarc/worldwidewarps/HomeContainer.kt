90b9449d060ea99d54a6c5949d4ad26ba6e96df1
Mizarc <mizarc@protonmail.com>
2022-01-06 17:40:15 +1000

    Add getter functions to home container
    Allows getting homes based on player, position, or fetching all at once.

--- src/main/kotlin/xyz/mizarc/worldwidewarps/HomeContainer.kt
+++ src/main/kotlin/xyz/mizarc/worldwidewarps/HomeContainer.kt
@@ -1,27 +1,78 @@
 package xyz.mizarc.worldwidewarps
 
 import co.aikar.idb.Database
+import org.bukkit.Bukkit
 import org.bukkit.DyeColor
 import xyz.mizarc.solidclaims.claims.Position
 import java.util.*
 import kotlin.collections.ArrayList
 
-class HomeContainer(private val database: Database, playerState: PlayerState) {
+class HomeContainer(private val database: Database) {
     private val homes = ArrayList<Home>()
 
-    init {
-        val results = database.getResults("SELECT * FROM homes WHERE playerId=?;", playerState.player.uniqueId)
-        for (result in results) {
-            homes.add(Home(
-                UUID.fromString(result.getString("id")), result.getString("name"),
-                DyeColor.valueOf(result.getString("colour")),
-                Position(result.getInt("positionX"),
-                    result.getInt("positionY"), result.getInt("positionZ"))))
+    fun getAll(): ArrayList<Home> {
+        val foundHomes = ArrayList<Home>()
+        foundHomes.addAll(homes)
+
+        if (foundHomes.isEmpty()) {
+            val results = database.getResults("SELECT * FROM homes;")
+            for (result in results) {
+                foundHomes.add(Home(
+                    UUID.fromString(result.getString("id")),
+                    Bukkit.getOfflinePlayer(UUID.fromString(result.getString("playerId"))),
+                    result.getString("name"), DyeColor.valueOf(result.getString("colour")),
+                    Position(result.getInt("positionX"),
+                        result.getInt("positionY"), result.getInt("positionZ"))))
+            }
         }
+
+        return foundHomes
     }
 
-    fun getAll(): ArrayList<Home> {
-        return homes
+    fun getByPlayer(playerState: PlayerState): ArrayList<Home> {
+        val foundHomes = ArrayList<Home>()
+        for (home in homes) {
+            if (home.player == Bukkit.getOfflinePlayer(playerState.player.uniqueId)) {
+                foundHomes.add(home)
+            }
+        }
+
+        if (foundHomes.isEmpty()) {
+            val results = database.getResults("SELECT * FROM homes WHERE playerId=?;", playerState.player.uniqueId)
+            for (result in results) {
+                foundHomes.add(Home(
+                    UUID.fromString(result.getString("id")),
+                    Bukkit.getOfflinePlayer(UUID.fromString(result.getString("playerId"))),
+                    result.getString("name"), DyeColor.valueOf(result.getString("colour")),
+                    Position(result.getInt("positionX"),
+                        result.getInt("positionY"), result.getInt("positionZ"))))
+            }
+        }
+
+        return foundHomes
+    }
+
+    fun getByPosition(position: Position): ArrayList<Home> {
+        val foundHomes = ArrayList<Home>()
+        for (home in homes) {
+            if (home.position == position) {
+                foundHomes.add(home)
+            }
+        }
+
+        if (foundHomes.isEmpty()) {
+            val results = database.getResults("SELECT * FROM homes WHERE position.x AND position.y AND position.z")
+            for (result in results) {
+                foundHomes.add(Home(
+                    UUID.fromString(result.getString("id")),
+                    Bukkit.getOfflinePlayer(UUID.fromString(result.getString("playerId"))),
+                    result.getString("name"), DyeColor.valueOf(result.getString("colour")),
+                    Position(result.getInt("positionX"),
+                        result.getInt("positionY"), result.getInt("positionZ"))))
+            }
+        }
+
+        return foundHomes
     }
 
     fun add(home: Home) {
