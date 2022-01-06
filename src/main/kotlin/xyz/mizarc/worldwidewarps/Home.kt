7f378ae3617a32a1c465011987ee97181ef51369
Mizarc <mizarc@protonmail.com>
2022-01-06 17:21:09 +1000

    Add OfflinePlayer instance to home
    This allows a player instance to be linked to the home, as multiple
    players are able to set the same bed as a home.

--- src/main/kotlin/xyz/mizarc/worldwidewarps/Home.kt
+++ src/main/kotlin/xyz/mizarc/worldwidewarps/Home.kt
@@ -1,12 +1,16 @@
 package xyz.mizarc.worldwidewarps
 
 import org.bukkit.DyeColor
+import org.bukkit.OfflinePlayer
 import xyz.mizarc.solidclaims.claims.Position
 import java.util.*
 
 /**
  * Stores a home consisting of a name and a position.
+ * @property id The unique identifier.
+ * @property player The player that owns the home.
  * @property name The name of the home.
+ * @property colour The colour of the bed.
  * @property position The position in the world.
  */
-class Home(val id: UUID, val name: String, val colour: DyeColor, val position: Position)
+class Home(val id: UUID, val player: OfflinePlayer, val name: String, val colour: DyeColor, val position: Position)
