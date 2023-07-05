68c20e0ce16fc446ae7f0c78243dd6840cb8c423
Mizarc <mizarc@proton.me>
2023-07-06 01:43:31 +1000

    Add warp data object and repository
    Warps will be utilised as a public teleportation system. While warps
    will be owned and managed by a sole player, anyone is able to access
    these warps and teleport to them.

---
+++ src/main/kotlin/xyz/mizarc/worldwidewarps/Warp.kt
@@ -0,0 +1,44 @@
+package xyz.mizarc.worldwidewarps
+
+import org.bukkit.OfflinePlayer
+import org.bukkit.World
+import org.bukkit.entity.Player
+import java.util.*
+
+/**
+ * Stores a warp, a public global teleportation system.
+ * @property id The unique identifier.
+ * @property player The player that owns the warp.
+ * @property name The name of the warp.
+ * @property world The world the warp is in.
+ * @property position The position in the world.
+ * @property direction The facing direction
+ */
+class Warp(val id: UUID, val player: OfflinePlayer, val name: String, val world: World,
+           val position: Position, val direction: Direction) {
+
+    /**
+     * Used to create a new warp instance with an auto generated UUID.
+     * @param player The player that owns the warp.
+     * @param name The name of the warp.
+     * @param world The world the warp is in.
+     * @param position The position in the world.
+     */
+    constructor(player: OfflinePlayer, name: String, world: World, position: Position, direction: Direction):
+            this(UUID.randomUUID(), player, name, world, position, direction)
+
+    constructor(builder: Builder):
+            this(UUID.randomUUID(), builder.player, builder.name, builder.world, builder.position, builder.direction)
+
+    class Builder(val player: Player, val world: World, val position: Position) {
+        var name = ""
+        var direction = Direction.NORTH
+
+        fun name(name: String): Builder {
+            this.name = name
+            return this
+        }
+
+        fun build() = Warp(this)
+    }
+}
---
+++ src/main/kotlin/xyz/mizarc/worldwidewarps/WarpRepository.kt
@@ -0,0 +1,108 @@
+package xyz.mizarc.worldwidewarps
+
+import co.aikar.idb.Database
+import org.bukkit.Bukkit
+import java.util.*
+import kotlin.collections.ArrayList
+
+class WarpRepository(private val database: Database, private val players: PlayerRepository) {
+    private val warps = ArrayList<Warp>()
+
+    fun getAll(): ArrayList<Warp> {
+        val foundWarps = ArrayList<Warp>()
+        foundWarps.addAll(warps)
+
+        if (foundWarps.isEmpty()) {
+            val results = database.getResults("SELECT * FROM warps;")
+            for (result in results) {
+                val world = Bukkit.getWorld(UUID.fromString(result.getString("worldId"))) ?: continue
+                foundWarps.add(Warp(
+                    UUID.fromString(result.getString("id")),
+                    Bukkit.getOfflinePlayer(UUID.fromString(result.getString("playerId"))),
+                    result.getString("name"), world,
+                    Position(result.getInt("positionX"), result.getInt("positionY"),
+                        result.getInt("positionZ")), Direction.values()[result.getInt("direction")]))
+            }
+        }
+        return foundWarps
+    }
+
+    fun getByPlayer(playerState: PlayerState): ArrayList<Warp> {
+        val foundWarps = ArrayList<Warp>()
+        for (warp in warps) {
+            if (warp.player == Bukkit.getOfflinePlayer(playerState.player.uniqueId)) {
+                foundWarps.add(warp)
+            }
+        }
+
+        if (foundWarps.isEmpty()) {
+            val results = database.getResults("SELECT * FROM warps WHERE playerId=?;",
+                playerState.player.uniqueId)
+
+            for (result in results) {
+                val world = Bukkit.getWorld(UUID.fromString(result.getString("worldId"))) ?: continue
+                val warp = Warp(UUID.fromString(result.getString("id")),
+                    Bukkit.getOfflinePlayer(UUID.fromString(result.getString("playerId"))),
+                    result.getString("name"), world, Position(result.getInt("positionX"),
+                        result.getInt("positionY"), result.getInt("positionZ")),
+                        Direction.values()[result.getInt("direction")])
+                foundWarps.add(warp)
+                warps.add(warp)
+            }
+        }
+        return foundWarps
+    }
+
+    fun getByPosition(position: Position): ArrayList<Warp> {
+        val foundWarps = ArrayList<Warp>()
+        for (warp in warps) {
+            if (warp.position == position) {
+                foundWarps.add(warp)
+            }
+        }
+
+        if (foundWarps.isEmpty()) {
+            val results = database.getResults(
+                "SELECT * FROM homes WHERE positionX=? AND positionY=? AND positionZ=?",
+                position.x, position.y, position.z)
+
+            for (result in results) {
+                val world = Bukkit.getWorld(UUID.fromString(result.getString("worldId"))) ?: continue
+                foundWarps.add(Warp(UUID.fromString(result.getString("id")),
+                    Bukkit.getOfflinePlayer(UUID.fromString(result.getString("playerId"))),
+                    result.getString("name"), world, Position(result.getInt("positionX"),
+                        result.getInt("positionY"), result.getInt("positionZ")),
+                    Direction.values()[result.getInt("direction")]))
+            }
+        }
+        return foundWarps
+    }
+
+    fun add(warp: Warp) {
+        warps.add(warp)
+        database.executeInsert("INSERT INTO warps (id, playerId, name, worldId, " +
+                "positionX, positionY, positionZ, direction) VALUES (?, ?, ?, ?, ?, ?, ?, ?);",
+            warp.id, warp.player.uniqueId, warp.name, warp.world.uid,
+            warp.position.x, warp.position.y, warp.position.z, warp.direction)
+    }
+
+    fun update(warp: Warp) {
+        for (storedWarp in getByPlayer(players.getByPlayer(Bukkit.getPlayer(warp.player.uniqueId)!!)!!)) {
+            if (storedWarp.id == warp.id) {
+                warps.remove(storedWarp)
+                warps.add(warp)
+
+                database.executeUpdate("UPDATE warps SET playerId=?, name=?, worldId=?, " +
+                        "positionX=?, positionY=?, positionZ=?, direction=? WHERE id=?",
+                    warp.player.uniqueId, warp.name, warp.world.uid, warp.position.x, warp.position.y,
+                    warp.position.z, warp.direction.ordinal, warp.id)
+                return
+            }
+        }
+    }
+
+    fun remove(warp: Warp) {
+        warps.remove(warp)
+        database.executeUpdate("DELETE FROM warps WHERE id=?", warp.id)
+    }
+}
