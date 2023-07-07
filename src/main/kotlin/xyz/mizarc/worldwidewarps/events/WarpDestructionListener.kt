ac09a274edbdcb7c974532d6985592195cd16627
Mizarc <mizarc@proton.me>
2023-07-07 14:00:25 +1000

    Add warp destruction listener
    This removes the warp from the database when the block is destroyed.

---
+++ src/main/kotlin/xyz/mizarc/worldwidewarps/events/WarpDestructionListener.kt
@@ -0,0 +1,20 @@
+package xyz.mizarc.worldwidewarps.events
+
+import org.bukkit.Material
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.block.BlockBreakEvent
+import xyz.mizarc.worldwidewarps.Position
+import xyz.mizarc.worldwidewarps.WarpRepository
+
+class WarpDestructionListener(val warpRepository: WarpRepository): Listener {
+    @EventHandler
+    fun onWarpBreak(event: BlockBreakEvent) {
+        if (event.block.type != Material.LODESTONE) return
+
+        val existingWarp = warpRepository.getAll().find { it.position == Position(event.block.location) }
+        if (existingWarp != null) {
+            warpRepository.remove(existingWarp)
+        }
+    }
+}
